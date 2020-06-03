b2b03481ea3da8224606de7839e3258153fdc011
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-06-03 16:33:48 +0530

    basic project setup

---
+++ src/Router.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router';
+
+import ProtectedRoute from './routers/RouteProtected';
+import RegistrationRoute from './routers/RegistrationRoute';
