e1ffd0b7a262b59936d301e04b2123732ab80aed
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2020-06-09 14:02:29 +0530

    slightly improved cartItem card

--- client/src/components/cards/CartItem.jsx
+++ client/src/components/cards/CartItem.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import { Grid, Typography, Paper, Button } from "@material-ui/core";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import RemoveShoppingCartTwoToneIcon from '@material-ui/icons/RemoveShoppingCartTwoTone';
+import ArrowForwardIosTwoToneIcon from '@material-ui/icons/ArrowForwardIosTwoTone';
 
 export default function MediaCard() {
   const theme = useTheme();
   const useStyles = makeStyles({
     root: {
       transition: 'box-shadow .3s ease-in-out',
+      padding: theme.spacing(2),
       '&:hover': {
         boxShadow: theme.shadows[8],
       }
-    },
-    paddingContent: {
-      padding: theme.spacing(1)
     },
     itemDesc: {
       flexGrow: 1
@@ -36,7 +35,7 @@ export default function MediaCard() {
 
   return (
     <Paper elevation={3} className={classes.root}>
-      <Grid container justify="space-between">
+      <Grid container justify="space-between" spacing={2}>
         <Grid className={classes.imageWrapper} item>
           <img
             src="https://thumbor.forbes.com/thumbor/2441x2240/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1176382466%2F0x0.jpg%3FcropX1%3D2439%26cropX2%3D4880%26cropY1%3D307%26cropY2%3D2547"
@@ -45,46 +44,54 @@ export default function MediaCard() {
           />
         </Grid>
         <Grid item className={classes.itemDesc}>
-          <Grid container className={classes.paddingContent} direction="column">
-            <Grid item>
-              <Typography variant="h4">Item name</Typography>
-            </Grid>
-            <Grid item>
-              <Grid container direction="row">
-                <Grid item xs={4}>
-                  <Typography variant="h6">Variant</Typography>
+          <Grid container>
+            <Grid item className={classes.itemDesc} >
+              <Grid container direction="column">
+                <Grid item>
+                  <Typography variant="h4">Item name</Typography>
                 </Grid>
-                <Grid item xs={4}>
-                  <Typography variant="h6">Variant</Typography>
+                <Grid item>
+                  <Grid container direction="row">
+                    <Grid item xs={4}>
+                      <Typography variant="h6">Variant</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography variant="h6">Variant</Typography>
+                    </Grid>
+                    <Grid item xs={4}>
+                      <Typography variant="h6">Variant</Typography>
+                    </Grid>
+                  </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                  <Typography variant="h6">Variant</Typography>
+                <Grid item>
+                  <Typography variant="body1">
+                    Fine jersey Knit,100% bio washnteed
+              </Typography>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <Typography variant="body1">
-                Fine jersey Knit,100% bio washnteed
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-        <Grid className={classes.paddingContent} item>
-          <Grid container direction="column"
-            justify="space-between"
-            alignItems="flex-end">
             <Grid item>
-              <Typography variant="h5">$400</Typography>
-            </Grid>
-            <Grid item>
-              <Button variant="outlined" color="primary" size="medium" startIcon={<RemoveShoppingCartTwoToneIcon />}>
-                Remove
-              </Button>
-            </Grid>
-            <Grid item>
-              <Button variant="outlined" color="primary" size="medium" startIcon={<RemoveShoppingCartTwoToneIcon />}>
-                Buy Now
-              </Button>
+              <Grid container>
+                <Grid item>
+                  <Grid container spacing={2} direction="column"
+                    justify="space-between"
+                    alignItems="flex-end">
+                    <Grid item>
+                      <Typography variant="h5">$400</Typography>
+                    </Grid>
+                    <Grid item>
+                      <Button variant="outlined" color="primary" size="medium" fullWidth startIcon={<RemoveShoppingCartTwoToneIcon />}>
+                        Remove
+                      </Button>
+                    </Grid>
+                    <Grid item>
+                      <Button variant="outlined" color="primary" size="medium" fullWidth startIcon={<ArrowForwardIosTwoToneIcon />}>
+                        Buy Now
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
