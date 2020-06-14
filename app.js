5e5c7eabc1508aaf408c054a532c3a79f71b5e1c
“Santhosh” <santhoshvelr@gmail.com>
2020-06-14 09:58:50 +0530

    start script added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require('express-session');
+const path = require('path');
 const passport = require("passport");
 const cors = require("cors");
 
